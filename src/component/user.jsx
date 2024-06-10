0ce45c041200be152bb990f1d429f48124aa1368
teeerapong2020 <teerapong.chue@gmail.com>
2024-06-10 20:35:54 +0700

    semi-final

--- src/component/user.jsx
+++ src/component/user.jsx
@@ -1,28 +1,16 @@
-import React from 'react'
+import React from "react";
 
- const User = () => {
+const User = (users) => {
   return (
     <>
-   
-    <h2 className='mt-16 flex justify-center font-black text-4xl'>Generation Thailand</h2>
-    <h2 className='flex justify-center font-black text-4xl'>Home - User Sector</h2>
-    <div className='mt-16 flex justify-center gap-24'>
-        <button className='font-black bg-gray-400'>User Home Sector</button>
-        <button className='font-black bg-gray-400'>Admin Home Sector</button>
-    </div>
-    <table className='border-2 mt-16 border-slate-950 '>
-        <tr className='border-2	border-slate-950	'>
-            <th >Name</th>
-            <th>Last Name</th>
-            <th>Position</th>
-        </tr>
-        <tr className='border-2 border-slate-950	'>
-            <td>k</td>
-            <td>k</td>
-            <td>k</td>
-        </tr>
-    </table>
+      {users.map((member) => (
+        <div key={member.id} className="mx-16 flex gap-24">
+          <h2 className="text-2xl font-black">{member.name}</h2>
+          <h2 className="text-2xl font-black">{member.lastName}</h2>
+          <h2 className="text-2xl font-black">{member.position}</h2>
+        </div>
+      ))}
     </>
-  )
-}
-export default User
+  );
+};
+export default User;
